1807fca2b917fdce0e021b411ccbfa47454e518a
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 21:04:33 +0700

    first commit

---
+++ src/pbo10118045latihan10logika/PBO10118045Latihan10Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan10logika;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Logika
+ */
+public class PBO10118045Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " +dewasa);
+    }
+    
+}
